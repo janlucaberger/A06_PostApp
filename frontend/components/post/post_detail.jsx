8e0d606e84c2c538f4df3b90d0528159b3f9a208
Jan-Luca Berger <janluca@officeconduct.com>
2017-08-18 23:30:55 -0400

    Create a posts list component

--- frontend/components/post/post_detail.jsx
+++ frontend/components/post/post_detail.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
 import PostCreate from './post_create'
 
-class PostDetail extends React.Component{
-
-  componentDidMount(){
-    this.props.fetchPost(this.props.postId)
-  }
-
-  render(){
-    if(this.props.post){
-      return (
-        <div className="post-detail-container">
-          <PostCreate />
-        </div>
-      )
-    } else {
-      return(
-        <div>No post</div>
-      )
-    }
-  }
-
-
-}
+// class PostDetail extends React.Component{
+//
+//   componentDidMount(){
+//     this.props.fetchPost(this.props.postId)
+//   }
+//
+//   render(){
+//     if(this.props.post){
+//       return (
+//         <div className="post-detail-container">
+//           <PostCreate />
+//         </div>
+//       )
+//     } else {
+//       return(
+//         <div>No post</div>
+//       )
+//     }
+//   }
+//
+//
+// }
 
 export default PostDetail
