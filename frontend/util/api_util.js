d0a135aa2ab614ca15c0e7ffaa3f8a1dee83e6a7
Jan-Luca Berger <janluca@officeconduct.com>
2017-08-18 21:00:49 -0400

    Create API utility

---
+++ frontend/util/api_util.js
@@ -0,0 +1,40 @@
+export const fetchPost = (postId) => {
+	return $.ajax({
+		method: "GET",
+		url: `/api/posts/${postId}`
+	})
+}
+
+export const fetchAllPosts = () => {
+	return $.ajax({
+		method: "GET",
+		url: `/api/posts`
+	})
+}
+
+export const createPost = (post) => {
+	return $.ajax({
+		method: "POST",
+		url: "/api/posts",
+		data: {
+			post
+		}
+	})
+}
+
+export const updatePost = (post) => {
+	return $.ajax({
+		method: "PATCH",
+		url: "/api/posts",
+		data: {
+			post
+		}
+	})
+}
+
+export const deletePost = (postId) => {
+	return $.ajax({
+		method: "DELETE",
+		url: `/api/posts/${postId}`,
+	})
+}
