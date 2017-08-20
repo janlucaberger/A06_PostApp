71f4406beddbcc286441a4cd8a76dec3f6a6fd20
Jan-Luca Berger <janluca@officeconduct.com>
2017-08-19 20:30:22 -0400

    Refactor PostDetail into connected component

--- frontend/components/post/post_detail.jsx
+++ frontend/components/post/post_detail.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { fetchPost } from '../../actions/post_actions'
 
 class PostDetail extends React.Component {
 
-  // componentDidMount(){
-  //   this.props.fetchPost(this.props.postId)
-  // }
+  componentDidMount(){
+    this.props.fetchPost(this.props.postId)
+  }
 
   render(){
     if (this.props.post){
       return(
         <div className="post-detail-container">
           <h3>{this.props.post.title}</h3>
-          <div dangerouslySetInnerHTML={{ __html: this.props.post.body }} />
-          <Link to={`/posts/${this.props.post.id}`}>Go to Post</Link>
+          <div className="post-detail-body" dangerouslySetInnerHTML={{ __html: this.props.post.body }} />
+          <Link to="/">
+            <button className="button">Back to posts</button>
+          </Link>
         </div>
       )
     } else {
@@ -22,4 +26,18 @@ class PostDetail extends React.Component {
   }
 
 }
-export default PostDetail
+
+const mapStateToProps = (state, ownProps) => {
+  const postId = ownProps.match.params.postId
+  return{
+    post: state.posts[postId],
+    postId: postId
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchPost: (postId) => dispatch(fetchPost(postId))
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(PostDetail)
