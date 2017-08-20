492e0778b90fe0a9f5746da030c84813b71fa7fd
Jan-Luca Berger <janluca@officeconduct.com>
2017-08-19 20:24:41 -0400

    Refactor PostCreate as container wrapping _PostForm

--- frontend/components/post/post_create.jsx
+++ frontend/components/post/post_create.jsx
@@ -1,63 +1,21 @@
-import React from 'react';
-import ReactQuill from 'react-quill';
 import { connect } from 'react-redux'
 import { createPost } from '../../actions/post_actions'
+import PostCreate from './_post_form';
 
-class PostCreate extends React.Component{
-  constructor(props) {
-    super(props)
-    this.state = {
-      title: '',
-      body: '',
-    }
-    this.handleInput = this.handleInput.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.handleChange = this.handleChange.bind(this)
-  }
-
-  handleChange(value) {
-    this.setState({ body: value })
-  }
-
-  handleInput(e){
-    this.setState({
-      title: e.currentTarget.value
-    })
-  }
-
-  handleSubmit(e){
-    debugger
-    e.preventDefault();
-    this.props.createPost(this.state);
-  }
-
-  render(){
-    console.log(this.state)
-    return(
-      <div>
-        <div className="component-header">
-          <h3>Create a new post!</h3>
-        </div>
-        <form>
-          <input className="text-input" type="text" onChange={this.handleInput} placeholder="Title"/>
-          <ReactQuill
-            value={this.state.body}
-            onChange={this.handleChange}
-          />
-          <button className="button bottom-right" onClick={this.handleSubmit}>Create Post</button>
-        </form>
-      </div>
-    )
+const mapStateToProps = state => {
+  return {
+    title: "",
+    body: "",
+    id: null,
+    buttonText: "Create Post",
+    afterAction: false
   }
 }
-
-
 
 const mapDispatchToProps = dispatch => {
   return {
-    createPost: (post) => dispatch(createPost(post))
+    formAction: (post) => dispatch(createPost(post))
   }
 }
 
-
-export default connect(null, mapDispatchToProps)(PostCreate)
+export default connect(mapStateToProps, mapDispatchToProps)(PostCreate)
